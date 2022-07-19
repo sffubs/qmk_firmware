6c52cd8f9318e8bb902c9a31e26029273314c7ec
Henry Bucklow <patches@elsie.org.uk>
2022-07-19 20:11:33 +0100

    Changed ctrl on right thumb cluster for symbol key.

--- keyboards/handwired/dactyl_manuform_gimbal/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform_gimbal/keymaps/default/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_Q,               LT(0, KC_W), KC_E,        KC_R,        KC_T,                 /**/ LT(0, KC_Y),       KC_U,                KC_I,    KC_O,    KC_P,    KC_BSPC,
      MT(MOD_LSFT, KC_A), KC_S,        KC_D,        KC_F,        KC_G,                 /**/ KC_H,              KC_J,                KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      MT(MOD_LCTL, KC_Z), LT(0, KC_X), LT(0, KC_C), LT(0, KC_V), KC_B,                 /**/ KC_N,              KC_M,                KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-     /**/                              MT(MOD_LCTL, KC_TAB), MT(MOD_LSFT, KC_SPC),      MT(MOD_RSFT, KC_SPC), MT(MOD_LCTL, KC_ENT),
+     /**/                              MT(MOD_LCTL, KC_TAB), MT(MOD_LSFT, KC_SPC),      MT(MOD_RSFT, KC_SPC), LT(_SYMBOL, KC_ENT),
      /**/                              MO(_NAV),             LT(_NUM, KC_SPC),          LT(_NUM, KC_SPC),     MO(_NAV),
                                        KC_LALT,              KC_LGUI,                   KC_RGUI,              KC_RALT
      ),
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /**/                       KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS
     ),
     [_SYMBOL] = LAYOUT(
-     KC_GRV,  KC_NO, KC_NO, KC_NO, KC_NO,     /**/ S(KC_9),    S(KC_0),    KC_NO,      KC_NO,     KC_NO,      KC_BSPC,
-     KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,     /**/ S(KC_LBRC), S(KC_RBRC), S(KC_MINS), S(KC_EQL), S(KC_SCLN), S(KC_QUOT),
-     KC_NUBS, KC_NO, KC_NO, KC_NO, KC_NO,     /**/ KC_LBRC,    KC_RBRC,    KC_MINS,    KC_EQL,    KC_NUHS,    KC_NO,
+     KC_GRV,  KC_NO, KC_NO, KC_NO, KC_NO,     /**/ S(KC_9),    S(KC_0),    S(KC_MINS), S(KC_EQL), S(KC_SCLN), KC_BSPC,
+     KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,     /**/ S(KC_LBRC), S(KC_RBRC), KC_MINS,    KC_EQL,    KC_NUHS,    S(KC_QUOT),
+     KC_NUBS, KC_NO, KC_NO, KC_NO, KC_NO,     /**/ KC_LBRC,    KC_RBRC,    S(KC_COMM), S(KC_DOT), KC_NO,      KC_NO,
                             KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS,
                             KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS,
                             KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS
